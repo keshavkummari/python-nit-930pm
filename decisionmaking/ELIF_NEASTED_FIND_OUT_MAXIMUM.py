35c6d17477ff5458dd2bc312220df9dfbe0aceaa
keshavkummari <keshav.kummari@gmail.com>
2018-11-01 22:52:33 +0530

    decisionMaking

---
+++ decisionmaking/ELIF_NEASTED_FIND_OUT_MAXIMUM.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+x = float(input("1st Number: "))
+y = float(input("2nd Number: "))
+z = float(input("3rd Number: "))
+
+if x > y:
+    if x > z:
+        maximum = x
+    else:
+        maximum = z
+else: 
+    if y > z:
+        maximum = y
+    else:
+        maximum = z
+
+print("The maximam value is: ", maximum)
---
+++ decisionmaking/simpleIF.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+#course_name="Python"
+
+if course_name:		# suite :
+   print("1 - Got a true expression value")
+   print("Course Name: Python")
+   print("Course Duration: 30-Hours")
+   print("Daily 1 Hr")
+   print(course_name)
